25f573ad860163bb2074af22bdeaa63ca0beb154
ZeroPaul <landerspaulzero@gmail.com>
2023-07-07 12:36:58 -0500

    exe one expansion

--- list_one.py
+++ list_one.py
@@ -8,3 +8,40 @@
 
 # Juan numero de letras 4
 # Ana numero de letras 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Expansion de ejercicio
+# Contar el numero de vocales y consonantes
+# Ordenar la lista alfabeticamente
+# Convertir todo el ejercicio en funcion
+# La salida por consola debe ser la siguiente:
+
+
+# ['Ana', Juan']
+# se ingreso 2 nombres y estan ordenandos
+
+# Nombre: Ana 
+# Numero de vocales: 2
+# Numero de consonantes: 1
+# Numero de letras: 3
+
+# Nombre: Juan
+# Numero de vocales: 2
+# Numero de consonantes: 2
+# Numero de letras: 4
